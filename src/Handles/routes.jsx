5765902ca5b160bbb8bb6a19f3c6b401ec7bf735
Kyle Yuen <45528039+kyleyuen@users.noreply.github.com>
2025-09-26 13:06:44 -0700

    Updated main UI framework to support multiple pages such as /home /skilltree

--- src/Handles/routes.jsx
+++ src/Handles/routes.jsx
@@ -1,15 +1,17 @@
 import { Routes, Route, Navigate } from "react-router-dom";
-import Home from "../pages/home.jsx";
-import SkillTree from "../pages/skilltree.jsx";
+import Home from "../pages/Home.jsx";
+import SkillTree from "../pages/SkillTree.jsx";
 
 export default function AppRoutes() {
   return (
     <Routes>
       {/* redirect / → /home */}
       <Route path="/" element={<Navigate to="/home" replace />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/skilltree" element={<SkillTree />} />
-      {/* optional: 404 route */}
+  <Route path="/home" element={<Home />} />
+  <Route path="/skilltree" element={<SkillTree />} />
+  {/* alias: support plural path /skilltrees as user requested */}
+  <Route path="/skilltrees" element={<SkillTree />} />
+  {/* optional: 404 route */}
       <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
